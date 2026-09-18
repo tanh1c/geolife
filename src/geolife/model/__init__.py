a30585b5a59d5b8eef15d4921aff18343148908e
TAnh <166195192+tanh1c@users.noreply.github.com>
2026-09-18 13:54:40 +0700

    export CP2 Home Office production APIs

--- src/geolife/model/__init__.py
+++ src/geolife/model/__init__.py
@@ -1,1 +1,13 @@
-"""Home/Office/POI inference package; production logic follows CP2 contract review and TDD."""
+"""Home/Office/POI inference package."""
+
+from geolife.model.home_office import (
+    HomeOfficeConfig,
+    build_semantic_locations,
+    infer_home_office,
+)
+
+__all__ = [
+    "HomeOfficeConfig",
+    "build_semantic_locations",
+    "infer_home_office",
+]
